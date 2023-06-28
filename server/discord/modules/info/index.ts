d84f9d3f8eede2511ad12a762b9572ec37a27cab
Garrett Keneau <gart@gart.sh>
2023-06-28 12:38:56 -0400

    trans react this guy

--- server/discord/modules/info/index.ts
+++ server/discord/modules/info/index.ts
@@ -1,6 +1,8 @@
+import { MessageType } from "discord.js";
 import { bot } from "../../core";
 import Module from "../../core/base/module";
 import StatusImage from "./stats";
+import Logger from "../../core/utils/logger";
 
 export default class InfoModule extends Module {
   public name = "info";
@@ -12,6 +14,27 @@ export default class InfoModule extends Module {
 
   public override async onLoad(): Promise<boolean> {
     StatusImage.init();
+
+    bot.client.on("messageCreate", async (message) => {
+      if (
+        message.type == MessageType.Reply &&
+        message.content.toLowerCase().includes("trans react this guy")
+      ) {
+        const replyMessage = await message.fetchReference();
+        const replyAuthor = replyMessage.author;
+
+        const messages = await message.channel.messages.fetch({ limit: 100, cache: true });
+
+        messages.forEach(async (message) => {
+          if (message.author.id !== replyAuthor.id) return;
+
+          await message.react("🏳️‍⚧️");
+        });
+
+        await message.react("✅");
+      }
+    });
+
     return true;
   }
 }
