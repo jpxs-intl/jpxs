2fad42d4112a6059b55d7b5e4c48062b18810b4f
Garrett Keneau <gart@gart.sh>
2023-06-28 19:12:19 -0400

    emoji bullshit pt2

--- server/discord/modules/info/index.ts
+++ server/discord/modules/info/index.ts
@@ -2,6 +2,7 @@ import { MessageType } from "discord.js";
 import { bot } from "../../core";
 import Module from "../../core/base/module";
 import StatusImage from "./stats";
+import Emoji from "../../../types/emoji/emoji";
 
 export default class InfoModule extends Module {
   public name = "info";
@@ -15,10 +16,7 @@ export default class InfoModule extends Module {
     StatusImage.init();
 
     bot.client.on("messageCreate", async (message) => {
-      if (
-        message.type == MessageType.Reply &&
-        (message.content.toLowerCase().includes("trans react"))
-      ) {
+      if (message.type == MessageType.Reply && message.content.toLowerCase().includes("trans react")) {
         const replyMessage = await message.fetchReference();
         const replyAuthor = replyMessage.author;
 
@@ -31,6 +29,38 @@ export default class InfoModule extends Module {
         });
 
         await message.react("✅");
+        return;
+      }
+
+      if (message.type == MessageType.Reply && /(.+) react/gi.test(message.content)) {
+        const tag = /(?<emoji>.+) react/gi.exec(message.content)?.groups?.emoji;
+
+        console.log(tag);
+
+        if (!tag) {
+          await message.react("❌");
+          return
+        }
+
+        const emoji = Emoji.getEmojiByName(tag)?.emoji;
+
+        console.log(emoji);
+
+        if (!emoji) {
+          await message.react("❌");
+          return
+        }
+
+        const replyMessage = await message.fetchReference();
+        const replyAuthor = replyMessage.author;
+
+        const messages = await message.channel.messages.fetch({ limit: 20, cache: true });
+
+        messages.forEach(async (message) => {
+          if (message.author.id !== replyAuthor.id) return;
+
+          await message.react(emoji);
+        });
       }
     });
 
