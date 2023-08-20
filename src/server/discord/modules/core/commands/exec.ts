0631aaa0d01b23c676a6bedd6264ecfad68bd298
Garrett Keneau <gart@gart.sh>
2023-08-20 18:47:37 -0400

    exec command

--- src/server/discord/modules/core/commands/exec.ts
+++ src/server/discord/modules/core/commands/exec.ts
@@ -1,34 +1,121 @@
-import { EmbedBuilder, PermissionFlagsBits } from "discord.js";
+import {
+  ActionRow,
+  ActionRowBuilder,
+  EmbedBuilder,
+  ModalBuilder,
+  PermissionFlagsBits,
+  TextInputBuilder,
+  TextInputComponent,
+  TextInputStyle,
+} from "discord.js";
 import SlashCommandBuilder from "../../../core/loaders/objects/customSlashCommandBuilder";
 import { time } from "../../../core/utils/time";
+import DataStorage from "../../../../data/dataStorage";
+import { bot } from "../../../core";
+import InstructionManager from "../../../../data/instruction/instructionManager";
+import ExecInstruction from "../../../../data/instruction/types/execInstruction";
 
 const Command = new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Pong!")
+  .setName("exec")
+  .setDescription("Run a command on a jpxs server")
   .setDMPermission(false)
   .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-  .setFunction(async (interaction) => {
-    const msg = await interaction.reply({
-      content: "Pong!",
-      fetchReply: true,
-    });
+  .addStringOption((option) =>
+    option
+      .setName("server")
+      .setDescription("The server to run the command on")
+      .setRequired(true)
+      .setAutocomplete(async (interaction, input) => {
+        const servers = Object.keys(DataStorage.serverData)
+          .map((key) => {
+            const server = DataStorage.serverData[key];
+            const masterServerData = DataStorage.servers.find((s) => s.id === server.serverId);
+            return {
+              serverId: server.serverId,
+              name: masterServerData?.name,
+            };
+          })
+          .filter((server) => server.name?.toLowerCase().includes(input.toLowerCase()));
 
-    await interaction.editReply({
-        content: `Pong!`,
-        embeds: [
-          new EmbedBuilder()
-            .setTitle("Pong!")
-            .setDescription(
-              [
-                `**Latency:** ${msg.createdTimestamp - interaction.createdTimestamp}ms`,
-                `**API Latency:** ${Math.round(interaction.client.ws.ping)}ms`,
-                `**Uptime:** ${time(interaction.client.uptime).toString()}`,
-              ].join("\n"))
-            .setColor("Random")
-            .setTimestamp()
-        ]
-            
-    })
+        return new Promise((resolve) => {
+          resolve(
+            servers
+              .map((server) => ({
+                name: server.name || "Unknown",
+                value: server.serverId,
+              }))
+              .slice(0, 25)
+          );
+        });
+      })
+  )
+  .addStringOption((option) =>
+    option
+      .setName("command")
+      .setDescription("The code to run, type 'modal' to open a modal to edit the code instead")
+      .setRequired(true)
+  )
+  .setFunction(async (interaction) => {
+    const serverId = interaction.options.getString("server", true);
+    const command = interaction.options.getString("command", true);
+
+    const server = DataStorage.serverData[serverId];
+
+    if (!server) {
+      await interaction.reply({
+        content: "Server not found",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    if (command === "modal") {
+      const modal = new ModalBuilder()
+        .setTitle("Edit code")
+        .setCustomId(`editCode-${interaction.id}`)
+        .setComponents(
+          new ActionRowBuilder<TextInputBuilder>().addComponents(
+            new TextInputBuilder()
+              .setCustomId("code")
+              .setPlaceholder("return 'Hello world!';")
+              .setRequired(true)
+              .setStyle(TextInputStyle.Paragraph)
+          )
+        );
+
+      bot.modalManager.registerModal(`editCode-${interaction.id}`, async (interaction) => {
+        const code = interaction.fields.getTextInputValue("code");
+
+        if (!code) {
+          await interaction.reply({
+            content: "Invalid code",
+            ephemeral: true,
+          });
+          return;
+        }
+
+        const result = await InstructionManager.addInstructionWithResponse(
+          new ExecInstruction(serverId, { code })
+        );
+
+        await interaction.reply({
+          content: "```" + result + "```",
+          ephemeral: false,
+        });
+      });
+
+      await interaction.showModal(modal);
+      return;
+    }
+
+    const result = await InstructionManager.addInstructionWithResponse(
+      new ExecInstruction(serverId, { code: command })
+    );
+
+    await interaction.reply({
+      content: "```" + result + "```",
+      ephemeral: false,
+    });
   });
 
 export default Command;
