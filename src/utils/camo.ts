71fadb5b0cc4883d7f7130722f3c3a51ac1fe865
Garrett Keneau <gart@gart.sh>
2024-02-22 01:02:35 -0500

    whitelist camo hosts

--- src/utils/camo.ts
+++ src/utils/camo.ts
@@ -2,7 +2,30 @@ import crypto from 'crypto';
 
 export default class Camo {
 
+    static cache = new Map<string, string>();
+    static whiteListedHosts = [
+        'imgur.com',
+        'i.imgur.com',
+        'steamcdn-a.akamaihd.net',
+        'cdn.akamai.steamstatic.com',
+        'assets.jpxs.io',
+    ]
+
     static generateCamoUrl(inputUrl: string) {
+
+        if (inputUrl === null) {
+            return null;
+        }
+
+        if (this.cache.has(inputUrl)) {
+            return this.cache.get(inputUrl);
+        }
+
+        const urlObj = new URL(inputUrl);
+        if (!this.whiteListedHosts.includes(urlObj.hostname)) {
+            return inputUrl;
+        }
+
         const hex = crypto.createHmac('sha1', process.env.CAMO_SECRET as string).update(inputUrl).digest('hex');
         const url = Buffer.from(inputUrl).toString('hex');
         return `https://camo.jpxs.io/${hex}/${url}`;
