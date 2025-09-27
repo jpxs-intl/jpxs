bc01d96257c59212c09aaba7ae59b6dde5ad4f90
Garrett Keneau <gart@gart.sh>
2025-09-27 23:45:40 +0000

    update footer

--- src/client/pages/home.tsx
+++ src/client/pages/home.tsx
@@ -171,6 +171,8 @@ export default async function HomePage(props: { session: AuthSession; path: stri
 				<p class="footer-text">
 					&copy; {new Date().getFullYear()} <span class="primary">JPXS</span>. All rights reserved.
 					<br />
+					JPXS is in a end-of-life state and will no longer be updated.
+					<br />
 					<span class="primary">JPXS</span> is not affiliated with Sub Rosa, Devolver Digital, or Cryptic Sea.
 				</p>
 			</div>
